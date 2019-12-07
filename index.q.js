196798e099cc3f6ec2d5a016784e6c86da854f8c
allys-m <51463868+allys-m@users.noreply.github.com>
2019-12-07 23:45:20 +0100

    Update index.q.js

--- index.q.js
+++ index.q.js
@@ -4,6 +4,12 @@
 let main = {};
 
 main.start = $(function(){
+  var fset = $('#fset'),
+      nav  = $('#nav');
+  
+  fset.on('mouseover', function(){
+    nav.css({ 'fontSize': 15px });
+  });
   
   alert("Juste a test ");
 })();
